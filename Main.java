5cb0acc23c5a421ca8ff36d93f61cd5258457082
federicorgz <113560011+federicorgz@users.noreply.github.com>
2022-09-27 22:58:34 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+	String s;
+	Scanner scanner = new Scanner(System.in);
+	  
     System.out.println("Enter a word:");
+    s=scanner.next();
+	System.out.print(s.length());
     
-    //Your code will return the length of the entered String
   }
 }
